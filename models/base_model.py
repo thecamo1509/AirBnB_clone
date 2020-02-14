583b8b39ad0af5f0354a1592bbde73874e985232
andreammgcol <amendez72@misena.edu.co>
2020-02-14 09:40:13 -0500

    task 4 args, kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -5,10 +5,26 @@ from datetime import datetime
 
 
 class BaseModel():
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+
+        if (kwargs):
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                elif key == 'created_at':
+                    self.created_at = datetime.strptime(value,
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == 'updated_at':
+                    self.updated_at = datetime.strptime(value,
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                else:
+                    if (key != '__class__'):
+                        setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
