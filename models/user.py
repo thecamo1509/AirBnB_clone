8e3e664348e2f64c600be20c89ea941ecd95aae5
andreammgcol <amendez72@misena.edu.co>
2020-02-15 16:32:16 -0500

    documentation

--- models/user.py
+++ models/user.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 
 from models.base_model import BaseModel
+"""
+Public class that inherit from BaseModel
+"""
 
 
 class User(BaseModel):
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    """ Class State
+        Attributes:
+        -----------
+        email: string
+        password: string
+        first_name: string
+        last_name: string
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
