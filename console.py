80466f44b1821e336197c4f3eb8152b5c495f27a
pafuentess <pafuentess@unal.edu.co>
2020-02-15 16:01:50 -0500

    console file

--- console.py
+++ console.py
@@ -3,11 +3,45 @@
 
 
 import cmd
+import models
+from models.base_model import BaseModel
 
+all_class = ["BaseModel"]
 
 class HBNBCommand(cmd.Cmd):
     
     prompt = "(hbnb) "
+
+    def do_create(self, inp):
+        words = str.split(inp)
+        if (len(words) == 0):
+            print("** class name missing **")
+            return (False)
+        if (words[0] not in all_class):
+            print("** class doesn't exist **")
+            return (False)
+        else:
+            instance = BaseModel()
+            instance.save()
+            print(instance.id)
+
+    def do_show(self, inp):
+        words = str.split(inp)
+        if (len(words) == 0):
+            print("** class name missing **")
+            return (False)
+        if (words[0] not in all_class):
+            print("** class doesn't exist **")
+            return (False)
+        if (len(words) < 2):
+            print("** instance id missing **")
+            return (False)
+        key = words[0] + "." + words[1]
+        if key in models.storage.all():
+            print(models.storage.all()[key])
+        else:
+            print("** no instance found **")
+
     def do_quit(self, inp):
         """Quit command to exit the program\n"""
         return True
