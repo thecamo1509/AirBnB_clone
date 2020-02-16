81752d95b38a221cd729862ff52fcbb7a92d7e8a
pafuentess <pafuentess@unal.edu.co>
2020-02-16 15:10:02 -0500

    console task 10

--- console.py
+++ console.py
@@ -6,8 +6,14 @@ import cmd
 import models
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
-all_class = {"BaseModel": BaseModel, "User": User}
+all_class = {"BaseModel": BaseModel, "User": User, "State" : State,
+        "City" : City, "Amenity" : Amenity, "Place" : Place, "Review": Review}
 
 class HBNBCommand(cmd.Cmd):
     
