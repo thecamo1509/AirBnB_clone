f363ba6389f8d174701724fa14ec310f08f87271
andreammgcol <amendez72@misena.edu.co>
2020-02-16 17:19:13 -0500

    documentation and pep8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,18 +4,35 @@ from ..base_model import BaseModel
 
 
 class FileStorage():
+    """
+    FileStorage class: that serializes and deserializes
+    a JSON file to instances
+    Attributes:
+    ----------
+    __file_path: string - path to the JSON file
+    __objects: dictionary - empty but will store all objects
+    """
     def __init__(self):
         self.__file_path = "file.json"
         self.__objects = {}
 
     def all(self):
+        """
+        Public method that returns the dictionary __objects
+        """
         return(self.__objects)
 
     def new(self, obj):
+        """
+        Public method that sets in __objects the obj with key
+        """
         c_n = obj.__class__.__name__ + "." + obj.id
         self.__objects[c_n] = obj
 
     def save(self):
+        """
+        Public method that serializes __objects to the JSON file
+        """
         new_object = {}
         for key, value in self.__objects.items():
             new_object.update({key: value.to_dict()})
@@ -24,11 +41,17 @@ class FileStorage():
             f.write(myjson)
 
     def delete(self, obj=None):
+        """
+        Public method that deletes an instance based on the class
+        """
         if obj is not None:
             del self.__objects[obj.__class__.__name__ + '.' + obj.id]
             self.save()
 
     def reload(self):
+        """
+        Public method that deserializes the JSON file to __objects
+        """
         if path.isfile(self.__file_path):
             with open(self.__file_path, "r") as f:
                 json_loads = json.load(f)
