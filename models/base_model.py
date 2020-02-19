b3f504f44c6d6db8e8a8460417642c85bff20fb0
pafuentess <pafuentess@unal.edu.co>
2020-02-19 11:10:29 -0500

    date an update fix

--- models/base_model.py
+++ models/base_model.py
@@ -36,7 +36,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             models.storage.new(self)
 
     def __str__(self):
@@ -51,6 +51,7 @@ class BaseModel:
         Method updates the public instance attribute updated_at
         with the current datetime
         """
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
@@ -60,8 +61,8 @@ class BaseModel:
         """
         new_dir = self.__dict__.copy()
         new_dir["__class__"] = self.__class__.__name__
-        new_dir["created_at"] = self.created_at.isoformat()
-        new_dir["updated_at"] = self.updated_at.isoformat()
+        new_dir["created_at"] = datetime.isoformat(self.created_at)
+        new_dir["updated_at"] = datetime.isoformat(self.updated_at)
         return(new_dir)
 
     def delete(self):
